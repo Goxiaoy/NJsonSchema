a41dbafba9c93db16d749b12c1201c6add545946
Goxiaoy <xy1996mail@gmail.com>
2019-10-14 13:01:28 +0800

    add test json

--- src/NJsonSchema.CodeGeneration.CSharp.Tests/SurveyJsTests.cs
+++ src/NJsonSchema.CodeGeneration.CSharp.Tests/SurveyJsTests.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
+using Newtonsoft.Json;
 using Xunit;
 using Xunit.Abstractions;
 using Xunit.Sdk;
@@ -31,29 +34,69 @@ namespace NJsonSchema.CodeGeneration.CSharp.Tests
             {
                 ClassStyle = CSharpClassStyle.Poco,
                 SchemaType = SchemaType.Swagger2,
-                DateType = "System.DateTime"
+                DateType = "System.DateTime",
+                Namespace = "Survey"
             });
             var code = generator.GenerateFile("SurveyJs");
             output.WriteLine(code);
             System.IO.File.WriteAllText("Survey.cs",code);
             //Test Compile
-            AssertCompile(code);
+            var type=AssertCompile(code);
+
+            var q1 = File.ReadAllText("References/q1.json");
+
+            var q1Survey=JsonConvert.DeserializeObject(q1, type);
         }
 
-        private static void AssertCompile(string code)
+        private static Type AssertCompile(string code)
         {
             var syntaxTree = CSharpSyntaxTree.ParseText(code);
             var errors = syntaxTree
                 .GetDiagnostics()
                 .Where(_ => _.Severity == DiagnosticSeverity.Error);
 
-            var sb = new StringBuilder();
-            foreach (var e in errors)
-            {
-                sb.AppendLine($"{e.Id} at {e.Location}: {e.GetMessage()}");
-            }
+            var assembly = GenerateAssembly(syntaxTree);
 
-            Assert.Empty(sb.ToString());
+            return assembly.GetType("Survey.SurveyJs");
+        }
+
+        private static Assembly GenerateAssembly(SyntaxTree syntaxTree)
+        {
+            string assemblyName = Path.GetRandomFileName();
+            var references = AppDomain.CurrentDomain.GetAssemblies().Where(p => !p.IsDynamic)
+                .Select(x => MetadataReference.CreateFromFile(x.Location));
+
+            CSharpCompilation compilation = CSharpCompilation.Create(
+                assemblyName,
+                syntaxTrees: new[] { syntaxTree },
+                references: references,
+                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            using (var ms = new MemoryStream())
+            {
+                EmitResult result = compilation.Emit(ms);
+
+                if (!result.Success)
+                {
+                    IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic =>
+                        diagnostic.IsWarningAsError ||
+                        diagnostic.Severity == DiagnosticSeverity.Error);
+
+                    var sb = new StringBuilder();
+                    foreach (var e in failures)
+                    {
+                        sb.AppendLine($"{e.Id} at {e.Location}: {e.GetMessage()}");
+                    }
+                    Assert.Empty(sb.ToString());
+                    return null;
+                }
+                else
+                {
+                    ms.Seek(0, SeekOrigin.Begin);
+                    Assembly assembly = Assembly.Load(ms.ToArray());
+                    return assembly;
+                }
+            }
         }
     }
 }
